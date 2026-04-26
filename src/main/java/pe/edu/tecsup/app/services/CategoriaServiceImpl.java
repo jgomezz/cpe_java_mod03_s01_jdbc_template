b63360f82605733f58fc03084c1977c98113fbc4
jgomezz <jgomezz@gmail.com>
2026-04-26 12:06:08 -0500

    add CategoriaService

---
+++ src/main/java/pe/edu/tecsup/app/services/CategoriaServiceImpl.java
@@ -0,0 +1,20 @@
+package pe.edu.tecsup.app.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.tecsup.app.entities.Categoria;
+import pe.edu.tecsup.app.repositories.CategoriaRepository;
+
+import java.util.List;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService{
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Override
+    public List<Categoria> findAll() {
+        return this.categoriaRepository.findAll();
+    }
+}
