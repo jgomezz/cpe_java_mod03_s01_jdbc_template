430b803d73ebe9e9b5f9eb6c61f6e39682794332
jgomezz <jgomezz@gmail.com>
2026-04-26 12:38:21 -0500

    update CategoriaServiceImpl

--- src/main/java/pe/edu/tecsup/app/services/CategoriaServiceImpl.java
+++ src/main/java/pe/edu/tecsup/app/services/CategoriaServiceImpl.java
@@ -1,17 +1,17 @@
 package pe.edu.tecsup.app.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pe.edu.tecsup.app.entities.Categoria;
 import pe.edu.tecsup.app.repositories.CategoriaRepository;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Service
 public class CategoriaServiceImpl implements CategoriaService{
 
-    @Autowired
-    private CategoriaRepository categoriaRepository;
+    private final CategoriaRepository categoriaRepository;
 
     @Override
     public List<Categoria> findAll() {
