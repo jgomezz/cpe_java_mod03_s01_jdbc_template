e6d0484832eb36eaea553535afa2d37a3f556415
jgomezz <jgomezz@gmail.com>
2026-04-26 12:15:50 -0500

    add CategoriaServiceTest

---
+++ src/test/java/pe/edu/tecsup/app/services/CategoriaServiceTest.java
@@ -0,0 +1,22 @@
+package pe.edu.tecsup.app.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoriaServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
