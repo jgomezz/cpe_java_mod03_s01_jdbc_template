acdd828a47e92d9a666bd0396fe67aa31bcd7e52
jgomezz <jgomezz@gmail.com>
2026-04-26 12:20:34 -0500

    update CategoriaServiceTest

--- src/test/java/pe/edu/tecsup/app/services/CategoriaServiceTest.java
+++ src/test/java/pe/edu/tecsup/app/services/CategoriaServiceTest.java
@@ -3,10 +3,19 @@ package pe.edu.tecsup.app.services;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pe.edu.tecsup.app.entities.Categoria;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CategoriaServiceTest {
+
+    @Autowired
+    private CategoriaService categoriaService;
 
     @BeforeEach
     void setUp() {
@@ -18,5 +27,8 @@ class CategoriaServiceTest {
 
     @Test
     void findAll() {
+
+        //List<Categoria> categorias = categoriaService.findAll();
+
     }
 }
